4f23a88f5f11ceee0e786a44b478f1da1f23bde8
Leon Dick <ldick@3-F-10.42heilbronn.de>
2025-10-30 14:01:29 +0100

    initial push part 2

---
+++ include/Client.hpp
@@ -0,0 +1,14 @@
+#ifndef CLIENT_HPP
+# define CLIENT_HPP
+
+#include <Server.hpp>
+
+class Client
+{
+	private:
+
+	public:
+	
+};
+
+#endif
---
+++ include/Server.hpp
@@ -0,0 +1,28 @@
+#ifndef SERVER_HPP
+# define SERVER_HPP
+
+#include <poll.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include "Client.hpp"
+
+class Server
+{
+	private:
+
+	public:
+};
+
+#endif
